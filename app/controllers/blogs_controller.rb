02d44fee622a830e6ae7dee1fda25de71323a325
Demetra Sklaviadis <demetra@Demetras-MacBook-Pro.local>
2018-06-15 11:35:09 -0400

    Integrate friendly routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -63,8 +63,9 @@ class BlogsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    # Friendly, still looks for parameters, pulls the slug, finds the id
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   # get 'pages/home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  get 'pages/contact'
+  # get 'pages/contact'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
